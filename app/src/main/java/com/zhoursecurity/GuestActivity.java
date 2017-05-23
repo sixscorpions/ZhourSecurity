ccf64f1a5cdf5c8cb9f2529e1498c8e9780bbaa2
Shankar Pilli <shankar.pilli@sparity.com>
2017-05-23 17:03:43 +0530

    Added vehicle scan in the guest

--- app/src/main/java/com/zhoursecurity/GuestActivity.java
+++ app/src/main/java/com/zhoursecurity/GuestActivity.java
@@ -1,11 +1,34 @@
 package com.zhoursecurity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.Text;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,15 +42,30 @@ public class GuestActivity extends Activity {
 
     @BindView(R.id.tv_message)
     TextView tvMessage;
+    @BindView(R.id.tv_scan_text)
+    TextView tvScanText;
 
     @BindView(R.id.btn_scan_vehicle)
     Button btnScanVehicle;
+
+    private static final int REQUEST_WRITE_PERMISSION = 20;
+
+    private static final String SAVED_INSTANCE_URI = "uri";
+    private static final String SAVED_INSTANCE_RESULT = "result";
+    private Uri imageUri;
+    private static final int PHOTO_REQUEST = 10;
+    private TextRecognizer detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest);
         ButterKnife.bind(this);
+        if (savedInstanceState != null) {
+            imageUri = Uri.parse(savedInstanceState.getString(SAVED_INSTANCE_URI));
+            tvScanText.setText(savedInstanceState.getString(SAVED_INSTANCE_RESULT));
+        }
+        detector = new TextRecognizer.Builder(getApplicationContext()).build();
     }
 
     @OnClick(R.id.btn_check)
@@ -36,7 +74,36 @@ public class GuestActivity extends Activity {
             etWriteYourCode.setText("");
             tvMessage.setVisibility(View.VISIBLE);
             btnScanVehicle.setVisibility(View.VISIBLE);
+            tvScanText.setVisibility(View.VISIBLE);
         }
+    }
+
+    @OnClick(R.id.btn_scan_vehicle)
+    void btnScanVehicle() {
+        ActivityCompat.requestPermissions(GuestActivity.this, new
+                String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_WRITE_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture();
+                } else {
+                    Toast.makeText(GuestActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
+        imageUri = FileProvider.getUriForFile(GuestActivity.this,
+                GuestActivity.this.getApplicationContext().getPackageName() + ".provider", photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, PHOTO_REQUEST);
     }
 
     private boolean isValidFields() {
@@ -51,4 +118,84 @@ public class GuestActivity extends Activity {
         return isValid;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PHOTO_REQUEST && resultCode == RESULT_OK) {
+            launchMediaScanIntent();
+            try {
+                Bitmap bitmap = decodeBitmapUri(this, imageUri);
+                if (detector.isOperational() && bitmap != null) {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<TextBlock> textBlocks = detector.detect(frame);
+                    String blocks = "";
+                    String lines = "";
+                    String words = "";
+                    for (int index = 0; index < textBlocks.size(); index++) {
+                        //extract scanned text blocks here
+                        TextBlock tBlock = textBlocks.valueAt(index);
+                        blocks = blocks + tBlock.getValue() + "\n" + "\n";
+                        for (Text line : tBlock.getComponents()) {
+                            //extract scanned text lines here
+                            lines = lines + line.getValue() + "\n";
+                            for (Text element : line.getComponents()) {
+                                //extract scanned text words here
+                                words = words + element.getValue() + ", ";
+                            }
+                        }
+                    }
+                    if (textBlocks.size() == 0) {
+                        tvScanText.setText("Scan Failed: Found nothing to scan");
+                    } else {
+                        tvScanText.setText("Number : ");
+                        tvScanText.setText(tvScanText.getText() + blocks + "\n");
+                        /*tvScanText.setText(tvScanText.getText() + "---------" + "\n");
+                        tvScanText.setText(tvScanText.getText() + "Lines: " + "\n");
+                        tvScanText.setText(tvScanText.getText() + lines + "\n");
+                        tvScanText.setText(tvScanText.getText() + "---------" + "\n");
+                        tvScanText.setText(tvScanText.getText() + "Words: " + "\n");
+                        tvScanText.setText(tvScanText.getText() + words + "\n");
+                        tvScanText.setText(tvScanText.getText() + "---------" + "\n");*/
+                    }
+                } else {
+                    tvScanText.setText("Could not set up the detector!");
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT)
+                        .show();
+                Log.e("Tag", e.toString());
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (imageUri != null) {
+            outState.putString(SAVED_INSTANCE_URI, imageUri.toString());
+            outState.putString(SAVED_INSTANCE_RESULT, tvScanText.getText().toString());
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    private void launchMediaScanIntent() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(imageUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver()
+                .openInputStream(uri), null, bmOptions);
+    }
 }
