37527644b687492bd8acaad49020aa26885a7490
Shankar Pilli <shankar.pilli@sparity.com>
2017-05-23 16:43:49 +0530

    Fixed issues

--- app/src/main/java/com/zhoursecurity/GuestActivity.java
+++ app/src/main/java/com/zhoursecurity/GuestActivity.java
@@ -1,9 +1,9 @@
 package com.zhoursecurity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,6 +20,9 @@ public class GuestActivity extends Activity {
     @BindView(R.id.tv_message)
     TextView tvMessage;
 
+    @BindView(R.id.btn_scan_vehicle)
+    Button btnScanVehicle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +35,7 @@ public class GuestActivity extends Activity {
         if (isValidFields()) {
             etWriteYourCode.setText("");
             tvMessage.setVisibility(View.VISIBLE);
+            btnScanVehicle.setVisibility(View.VISIBLE);
         }
     }
 
@@ -39,6 +43,9 @@ public class GuestActivity extends Activity {
         boolean isValid = true;
         if (Utility.isValueNullOrEmpty(etWriteYourCode.getText().toString())) {
             Utility.showToastMessage(this, "Please write code");
+            isValid = false;
+        } else if (etWriteYourCode.getText().toString().length() != 4) {
+            Utility.showToastMessage(this, "Code must be 4 digit");
             isValid = false;
         }
         return isValid;
